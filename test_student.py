fcd686bdce574943afde33e01e535203eee15a6b
Vu Anh <anh.vu2@mservice.com.vn>
2023-09-30 16:34:12 +0700

    inital commit

---
+++ test_student.py
@@ -0,0 +1,9 @@
+# create unittest for student.py
+import unittest
+
+from student import Student
+
+class TestStudent(unittest.TestCase):
+    def test_display(self):
+        student = Student("John", 16)
+        self.assertEqual(student.display(), "John is 16 years old")
